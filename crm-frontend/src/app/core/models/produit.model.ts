99b884c91ad8d36ba6d2617aeffb10426377dbcf
Fotso12 <tamofotso90@gmail.com>
2026-05-21 23:41:12 +0100

    Frontend

--- crm-frontend/src/app/core/models/produit.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Produit {
-  id?: number;
-  nom: string;
-  description?: string;
-  prixUnitaire: number;
-  categorie?: string;
-  disponible: boolean;
-  dateCreation?: Date;
-}
--- crm-frontend/src/app/core/models/restaurant.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Restaurant {
-  id?: number;
-  nom: string;
-  ville: string;
-  adresse?: string;
-  telephone?: string;
-  actif: boolean;
-  dateCreation?: Date;
-}
