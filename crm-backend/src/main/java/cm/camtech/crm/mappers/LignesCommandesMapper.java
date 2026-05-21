d7ccc7a5e13427273aa2df1d11ac939361e39b79
Fotso12 <tamofotso90@gmail.com>
2026-05-21 04:57:52 +0100

    Ajout des Mappers restant

--- crm-backend/src/main/java/cm/camtech/crm/mappers/LignesCommandesMapper.java
+++ crm-backend/src/main/java/cm/camtech/crm/mappers/LignesCommandesMapper.java
@@ -1,4 +1,18 @@
-package src.main.java.cm.camtech.crm.mappers;
+package cm.camtech.crm.mappers;
 
+import cm.camtech.crm.dtos.LignesCommandesDto;
+import cm.camtech.crm.entities.LignesCommandes;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface LignesCommandesMapper {
+
+    @Mapping(source = "commandeId.id", target = "commandeId")
+    @Mapping(source = "produitId.id", target = "produitId")
+    LignesCommandesDto toDto(LignesCommandes lignesCommandes);
+
+    @Mapping(source = "commandeId", target = "commandeId.id")
+    @Mapping(source = "produitId", target = "produitId.id")
+    LignesCommandes toEntity(LignesCommandesDto lignesCommandesDto);
 }
--- crm-backend/src/main/java/cm/camtech/crm/mappers/ProduitsMapper.java
+++ crm-backend/src/main/java/cm/camtech/crm/mappers/ProduitsMapper.java
@@ -1,4 +1,13 @@
-package src.main.java.cm.camtech.crm.mappers;
+package cm.camtech.crm.mappers;
 
+import cm.camtech.crm.dtos.ProduitsDto;
+import cm.camtech.crm.entities.Produits;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface ProduitsMapper {
+
+    ProduitsDto toDto(Produits produits);
+
+    Produits toEntity(ProduitsDto produitsDto);
 }
