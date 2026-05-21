cae605b88c59a9886a94b7963abae776234fb23d
Fotso12 <tamofotso90@gmail.com>
2026-05-21 05:06:15 +0100

    Ajout des Interfaces restant

--- crm-backend/src/main/java/cm/camtech/crm/services/interfaces/ReclamationsInterface.java
+++ crm-backend/src/main/java/cm/camtech/crm/services/interfaces/ReclamationsInterface.java
@@ -1,4 +1,18 @@
-package src.main.java.cm.camtech.crm.services.interfaces;
+package cm.camtech.crm.services.interfaces;
+
+import cm.camtech.crm.dtos.ReclamationsDto;
+
+import java.util.List;
 
 public interface ReclamationsInterface {
+
+    ReclamationsDto save(ReclamationsDto reclamationsDto);
+
+    ReclamationsDto update(Long id, ReclamationsDto reclamationsDto);
+
+    ReclamationsDto findById(Long id);
+
+    List<ReclamationsDto> findAll();
+
+    void delete(Long id);
 }
